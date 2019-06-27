1d7b6f18a1c20b63a0544ee8aa5d421f20da12db
Mastrud <blake.mastrud@williams.com>
2019-06-27 12:48:48 -0600

    Add icon to windows installer

--- buildWin.js
+++ buildWin.js
@@ -2,9 +2,10 @@ var electronInstaller = require('electron-winstaller');
 
 resultPromise = electronInstaller.createWindowsInstaller({
   appDirectory: './Releases/shialator-win32-x64/',
-  outputDirectory: '/packaged/build/installer64',
+  outputDirectory: './packaged/build/installer64',
   authors: 'Nully Studios',
-  exe: 'shialator.exe'
+  exe: 'shialator.exe',
+  setupIcon: './assets/icon.ico'
 });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
